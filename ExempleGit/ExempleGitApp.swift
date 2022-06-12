0f63de303b7a71111bbda642677c1bb5c838cb38
Nyl <95429032+NylNeuville@users.noreply.github.com>
2022-06-12 09:53:49 +0200

    Initial Commit

---
+++ ExempleGit/ExempleGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExempleGitApp.swift
+//  ExempleGit
+//
+//  Created by Nyl Neuville on 12/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExempleGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
